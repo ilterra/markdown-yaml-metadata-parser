6961ed9df2b1e0de4dcf89c974a48ffcfb8c9218
Alberto Terragni <alberto.terragni@sncegroup.ch>
2017-05-22 22:17:46 +0200

    Update correct document

--- __tests__/__fixtures__/document01.markdown
+++ __tests__/__fixtures__/document01.markdown
@@ -5,3 +5,7 @@ keywords: latin, ipsum
 ---
 
 Correct markdown document. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+---
+Correct markdown document. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+
+Correct markdown document. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
--- __tests__/__fixtures__/documents.js
+++ __tests__/__fixtures__/documents.js
@@ -8,6 +8,10 @@ author: Marcus Antonius
 keywords: latin, ipsum
 ---
 
+Correct markdown document. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+---
+Correct markdown document. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+
 Correct markdown document. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`;
 
 const metadata01 = {
