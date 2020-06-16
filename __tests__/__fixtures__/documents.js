288d5962cefb5ad92c1a46d3fd80e37947f89957
Alberto Terragni <alberto.terragni@sncegroup.ch>
2020-06-16 17:58:15 +0200

    Lint code with Standard style

--- __tests__/__fixtures__/documents.js
+++ __tests__/__fixtures__/documents.js
@@ -12,45 +12,45 @@ Correct markdown document. Vestibulum tortor quam, feugiat vitae, ultricies eget
 ---
 Correct markdown document. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
 
-Correct markdown document. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`;
+Correct markdown document. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`
 
 const metadata01 = {
   author: 'Marcus Antonius',
   keywords: 'latin, ipsum',
   title: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
-};
+}
 
 const document02 = `---
 title: Lorem ipsum dolor sit amet, consectetur adipiscing
 author: Marcus Antonius
 keywords: latin, ipsum
----`;
+---`
 
 const metadata02 = {
   author: 'Marcus Antonius',
   keywords: 'latin, ipsum',
   title: 'Lorem ipsum dolor sit amet, consectetur adipiscing'
-};
+}
 
 const document03 = `---
 title: Lorem ipsum dolor sit amet, consectetur adipiscing
 author: Marcus Antonius
 keywords: latin, ipsum
 
-No metadata end. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`;
+No metadata end. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`
 
 const document04 = `title: Lorem ipsum dolor sit amet, consectetur adipiscing
 author: Marcus Antonius
 keywords: latin, ipsum
 ---
 
-No metadata start. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`;
+No metadata start. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`
 
 const document05 = `---
 
 ---
 
-Empty metadata. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`;
+Empty metadata. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`
 
 const document06 = `Metadata not at the beginning of the document. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
 
@@ -58,11 +58,11 @@ const document06 = `Metadata not at the beginning of the document. Vestibulum to
 title: The murder of our hopes and dreams
 author: Master John
 keywords: comedy, movie
----`;
+---`
 
 const document07 = `# Document with no metadata
 
-Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`;
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`
 
 const document08 = `---
 deliberate yaml syntax error
@@ -70,7 +70,7 @@ author: Marcus Antonius
 keywords: latin, ipsum
 ---
 
-Metadata with YAML syntax error. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`;
+Metadata with YAML syntax error. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.`
 
 module.exports = {
   document01,
@@ -83,4 +83,4 @@ module.exports = {
   document06,
   document07,
   document08
-};
+}
--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -1,66 +1,66 @@
-const parser = require('../lib/parser');
-const source = require('./__fixtures__/documents');
+const parser = require('../lib/parser')
+const source = require('./__fixtures__/documents')
 
 /**
  * Mock js-yaml.
  * safeLoad: return empty object by default.
  */
-let yamlParser;
+let yamlParser
 
 beforeAll(() => {
   yamlParser = {
     safeLoad: jest.fn().mockImplementation(() => ({}))
-  };
-});
+  }
+})
 
 it('parses metadata in a syntactically correct markdown document', () => {
-  yamlParser.safeLoad.mockImplementationOnce(() => source.metadata01);
-  expect(parser(yamlParser)(source.document01)).toMatchSnapshot();
-});
+  yamlParser.safeLoad.mockImplementationOnce(() => source.metadata01)
+  expect(parser(yamlParser)(source.document01)).toMatchSnapshot()
+})
 
 it('parses metadata in a syntactically correct markdown document with no content', () => {
-  yamlParser.safeLoad.mockImplementationOnce(() => source.metadata02);
-  expect(parser(yamlParser)(source.document02)).toMatchSnapshot();
-});
+  yamlParser.safeLoad.mockImplementationOnce(() => source.metadata02)
+  expect(parser(yamlParser)(source.document02)).toMatchSnapshot()
+})
 
 it('returns no metadata in a document without the ending triple dashes', () => {
-  expect(parser(yamlParser)(source.document03)).toMatchSnapshot();
-});
+  expect(parser(yamlParser)(source.document03)).toMatchSnapshot()
+})
 
 it('returns no metadata in a document without the opening triple dashes', () => {
-  expect(parser(yamlParser)(source.document04)).toMatchSnapshot();
-});
+  expect(parser(yamlParser)(source.document04)).toMatchSnapshot()
+})
 
 it('returns no metadata in a document with empty metadata', () => {
-  expect(parser(yamlParser)(source.document05)).toMatchSnapshot();
-});
+  expect(parser(yamlParser)(source.document05)).toMatchSnapshot()
+})
 
 it('returns no metadata in a document with metadata placed at the end', () => {
-  expect(parser(yamlParser)(source.document06)).toMatchSnapshot();
-});
+  expect(parser(yamlParser)(source.document06)).toMatchSnapshot()
+})
 
 it('returns no metadata in a document without metadata', () => {
-  expect(parser(yamlParser)(source.document07)).toMatchSnapshot();
-});
+  expect(parser(yamlParser)(source.document07)).toMatchSnapshot()
+})
 
 it("throws TypeError if it doesn't receive a String", () => {
-  function parseNumber() {
-    parser(yamlParser)(123);
+  function parseNumber () {
+    parser(yamlParser)(123)
   }
 
   expect(parseNumber).toThrowError(
     TypeError('Source parameter (src) must be a string.')
-  );
-});
+  )
+})
 
 it('throws Error if metadata are syntactically incorrect', () => {
   yamlParser.safeLoad.mockImplementationOnce(() => {
-    throw new Error();
-  });
+    throw new Error()
+  })
 
-  function parseSyntacticallyIncorrectYAML() {
-    parser(yamlParser)(source.document08);
+  function parseSyntacticallyIncorrectYAML () {
+    parser(yamlParser)(source.document08)
   }
 
-  expect(parseSyntacticallyIncorrectYAML).toThrow();
-});
+  expect(parseSyntacticallyIncorrectYAML).toThrow()
+})
